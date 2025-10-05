128f7801569a87491265371259d11afcd2e35059
rohit-sonawane7 <rohitsonawane735@gmail.com>
2025-10-05 20:00:16 +0530

    health check api added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,21 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpRequestDto } from './dto/Request/signup.dto';
 import { LoginRequestDto } from './dto/Request/login.dto';
 import { SignUpResponseDto } from './dto/Response/signup.dto';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { ResetPasswordDto } from './dto/Request/reset-password.dto';
+import { ResponseMessageDto } from 'src/common/dto/Response/response-message.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
+
+  @Get('health-check')
+  @HttpCode(HttpStatus.OK)
+  healthCheck(): ResponseMessageDto {
+    return { message: "success" }
+  }
 
   @Post('signup')
   @HttpCode(HttpStatus.CREATED)
